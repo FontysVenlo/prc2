ffe103fab627939ea6908b8b24c44193b73052d7
Pieter van den Hombergh <pieter.van.den.hombergh@gmail.com>
2024-01-24 13:17:52 +0100

    better use of pattern matching

--- topics/code/sealedshapes/src/main/java/sealedshapes/BasicPoint.java
+++ topics/code/sealedshapes/src/main/java/sealedshapes/BasicPoint.java
@@ -2,37 +2,37 @@ package sealedshapes;
 
 /**
  * Helper class to define points and helper functions.
- * 
+ *
  * @author Pieter van den Hombergh {@code <pieter.van.den.hombergh@gmail.com>}
- * 
+ *
  * @param x coordinate of point
  * @param y coordinate of point
  */
-public record BasicPoint(double x, double y) {
+public record BasicPoint( double x, double y) {
 
     /**
-     * Distance between this BasicPoint and the other BasicPoint. 
-     * The computation uses Pythagoras' formula to do the computation.
-     * 
+     * Distance between this BasicPoint and the other BasicPoint. The
+     * computation uses Pythagoras' formula to do the computation.
+     *
      * @param other point
-     * @return length of the shortest straight line through this 
-     *         and the other point.
+     * @return length of the shortest straight line through this and the other
+     * point.
      */
-    public double distance(BasicPoint other) {
+    public double distance( BasicPoint other ) {
         double xLen = Math.abs( x - other.x );
         double yLen = Math.abs( y - other.y );
         return Math.hypot( xLen, yLen );
     }
-    
+
     /**
-     * Factory method to allow shorthand {@code bp(1,0D,2.0D)} 
-     *     to create a point. 
-     *     import static to have the most benefits.
+     * Factory method to allow shorthand {@code bp(1,0D,2.0D)} to create a
+     * point. import static to have the most benefits.
+     *
      * @param x coordinate
      * @param y coordinate
-     * @return  the record
+     * @return the record
      */
-    public static BasicPoint bp(double x, double y){
-        return new BasicPoint(x, y );
+    public static BasicPoint bp( double x, double y ) {
+        return new BasicPoint( x, y );
     }
 }
--- topics/code/sealedshapes/src/main/java/sealedshapes/BasicShape.java
+++ topics/code/sealedshapes/src/main/java/sealedshapes/BasicShape.java
@@ -1,12 +1,14 @@
 package sealedshapes;
 
+import static java.lang.Math.abs;
+
 /**
  * Example sealed intarface.
- * 
+ *
  * @author Pieter van den Hombergh {@code <pieter.van.den.hombergh@gmail.com>}
  */
 sealed interface BasicShape
-        extends Comparable<BasicShape> 
+        extends Comparable<BasicShape>
         permits Triangle, Rectangle, Circle {
 
     /**
@@ -16,19 +18,17 @@ sealed interface BasicShape
      */
     default double area() {
         return switch ( this ) {
-            case Circle(var unused, double radius) ->
+            case Circle( var unused, double radius) ->
                 radius * radius * Math.PI;
-            case Rectangle(BasicPoint topLeft, BasicPoint bottomRight) ->
-                Math.abs( topLeft.x() - bottomRight.x() )
-                * Math.abs( topLeft.y() - bottomRight.y() );
-            case Triangle(BasicPoint a, BasicPoint b, BasicPoint c) -> {
+            case Rectangle( BasicPoint( var tlx, var tly), BasicPoint( var brx, var bry)) ->
+                abs( tlx - brx ) * abs( tly - bry );
+            case Triangle( BasicPoint a, BasicPoint b, BasicPoint c) -> {
                 double lA = b.distance( c );
                 double lB = c.distance( a );
                 double lC = a.distance( b );
                 double s = ( lA + lB + lC ) / 2D;
                 yield Math.sqrt( s * ( s - lA ) * ( s - lB ) * ( s - lC ) );
             }
-
         };
     }
 
@@ -40,7 +40,7 @@ sealed interface BasicShape
      * @return the outcome
      */
     @Override
-    public default int compareTo(BasicShape other) {
+    public default int compareTo( BasicShape other ) {
         return Double.compare( this.area(), other.area() );
     }
 
